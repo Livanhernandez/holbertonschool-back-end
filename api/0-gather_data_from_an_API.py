e288509a854a2d624f14867eadeffd5c10cd8b88
Livanhernandez <livanhernandez9@gmail.com>
2024-04-16 10:55:56 -0400

    gather data

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" API REST """
+
+import requests
+from sys import argv
+
+
+def get_employee(id=None):
+    if len(argv) > 1:
+        try:
+            id = int(argv[1])
+        except ValueError:
+            return
+        
+    if isinstance(id, int):
+        base = "https://jsonplaceholder.typicode.com"
+        user = requests.get(f"{base}/users/{id}").json()
+        to_dos = requests.get(f"{base}/todos/?userId={id}").json()
+
+        if user and to_dos:
+            total_tasks = len(to_dos)
+            titles_completed = [task["title"]
+                                for task in to_dos
+                                if task["completed"]]
+            
+            tasks_completed = len(titles_completed)
+
+            print(
+                "Employee {} is done with tasks({}/{}):".format(
+                    user["name"], tasks_completed, total_tasks
+                )
+            )
+
+            for title in titles_completed:
+                print(f"\t {title}")
+
+if __name__ == "__main__":
+    get_employee()
